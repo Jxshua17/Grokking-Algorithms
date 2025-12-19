8f648125f5a14c21f50107f707eea895383aad42
Joshua <112779066+Jxshua17@users.noreply.github.com>
2025-12-19 14:56:18 +0100

    overloaded a method in SearchAlgorithms
    I overloaded the BinarySearch method so that ArrayList can also be a parameter option rather than just arrays.

--- src/main/java/SearchAlgorithms.java
+++ src/main/java/SearchAlgorithms.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SearchAlgorithms {
 
     public int BinarySearch(int [] sortedList, int item){
@@ -9,6 +11,32 @@ public class SearchAlgorithms {
         while (low<=high){
             int mid = (low + high)/2;
             int guess = sortedList[mid];
+
+            if (guess == item){
+                return mid;
+            } else if (guess > item){
+                high = mid + 1;
+            } else{
+                low = mid + 1;
+            }
+        }
+
+        return 0;
+
+    }
+
+
+    //i wanted to write something for a resizable array and i couldn't do that with the regular arrays so i made use of ArrayList
+    //I also had to use method overloading here so that i could have the same method that accepts an ArrayList rather than a regular Array.
+    public int BinarySearch(ArrayList<Integer> sortedList, int item){
+        //item is the number we are looking for and the sortedList is the haystack we are going through.
+
+        int low = 0;
+        int high = sortedList.size() - 1;
+
+        while (low<=high){
+            int mid = (low + high)/2;
+            int guess = sortedList.get(mid);
 
             if (guess == item){
                 return mid;
