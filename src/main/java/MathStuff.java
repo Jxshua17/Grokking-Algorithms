528707deb4f36e3ff272ce999d5e5c8bc8841fbc
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-01-07 15:33:21 +0100

    created a new class MathStuff
    in this class, i created a method called distanceBetweenTwoPoints that calculates the distance between two points in a cartesian plane. why did i do this? because i got bored and needed a break from trying to implement dijkstra's algorithm in java.
    
    i created a test for this but didn't stage it for this commit because expected and actual do not match at all. the last three digits of the expected from my laptop's calculator is 903 but according to the compiler, it is 908 which is weird. i think floating point. i have seen a short talking about that.

---
+++ src/main/java/MathStuff.java
@@ -0,0 +1,24 @@
+public class MathStuff {
+    //i got bored so instead of taking a break to do something else, here i am writing a method that allows me...
+    //...to calculate the distance between two points in a cartesian plane
+
+    public static double distanceBetweenTwoPoints(int [] a, int [] b){
+        int x = b[0] - a[0];
+        int y = b[1] - a[1];
+        int distance = MathStuff.square(x) + MathStuff.square(y);
+
+        return Math.sqrt(distance);
+    }
+
+    public static int square(int number){
+        return number*number;
+    }
+
+    public static void main(String[] args) {
+
+        //using the main to just do a simple test because the value i get when with a calculator is different from actual.
+        //floating point numbers, i suspect.
+
+        System.out.println(MathStuff.distanceBetweenTwoPoints(new int[]{2, 3}, new int[]{4, 5}));
+    }
+}
