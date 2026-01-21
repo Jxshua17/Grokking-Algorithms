b690db4dc6ade31a4164944e25cdb3a868102cea
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-01-21 16:42:02 +0100

    Reimplemented the factorial method
    In preparation for my attempt to write the permutation and combination methods, i have had to see if i can recall how to write a recursive function which is what you'll need to write a factorial function. i needed to rewrite it without checking just to see if i really understood recursion. i think i did. now, to write the permutation and combination methods.

--- src/main/java/MathStuff.java
+++ src/main/java/MathStuff.java
@@ -2,7 +2,7 @@ public class MathStuff {
     //i got bored so instead of taking a break to do something else, here i am writing a method that allows me...
     //...to calculate the distance between two points in a cartesian plane
 
-    public static double distanceBetweenTwoPoints(int [] a, int [] b){
+    public static double distanceBetweenTwoPoints(int[] a, int[] b) {
         int x = b[0] - a[0];
         int y = b[1] - a[1];
         int distance = MathStuff.square(x) + MathStuff.square(y);
@@ -10,8 +10,17 @@ public class MathStuff {
         return Math.sqrt(distance);
     }
 
-    public static int square(int number){
-        return number*number;
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static int factorial(int r) {
+
+        if (r == 1) {
+            return r;
+        } else {
+            return MathStuff.factorial(r - 1) * r;
+        }
     }
 
     public static void main(String[] args) {
@@ -20,5 +29,6 @@ public class MathStuff {
         //floating point numbers, i suspect.
 
         System.out.println(MathStuff.distanceBetweenTwoPoints(new int[]{2, 3}, new int[]{4, 5}));
+        System.out.println(MathStuff.factorial(5));
     }
 }
