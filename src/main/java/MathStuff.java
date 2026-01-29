52f5b01ecf5b4ca8783710da94fc02ea23c82aca
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-01-29 13:56:43 +0100

    Solved the Daily Epsilon of Math Calendar problem for Jan 28.
    I used code to solve this because i really couldn't see a way for me to solve this using algebra. Brute forced it. i have no idea what the runtime is. it makes me want to actually check to see how that shit is calculated because i assume that the machine that checks my code on leetcode is way different from my own personal IDE+pc.

--- src/main/java/MathStuff.java
+++ src/main/java/MathStuff.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MathStuff {
     //i got bored so instead of taking a break to do something else, here i am writing a method that allows me...
     //...to calculate the distance between two points in a cartesian plane
@@ -38,5 +40,52 @@ public class MathStuff {
 
         System.out.println(MathStuff.distanceBetweenTwoPoints(new int[]{2, 3}, new int[]{4, 5}));
         System.out.println(MathStuff.factorial(5));
+
+
+        //solving the daily math calender for day 28 in january.
+        //find the largest number AB where A340B is divisible by 88.
+        String number = "A340B";
+        System.out.println(number.length());
+
+        System.out.println(number.charAt(0));
+
+        String newNumber = number.replace('A', '1');
+        System.out.println(newNumber.charAt(0));
+
+        char [] chars;
+        int largestNumber = 0;
+
+        for (int i=1;i<10;i++){
+            for (int j=0;j<10;j++){
+                chars = number.toCharArray();
+
+                chars[0] = (char) (i + 48);
+                chars[4] = (char) (j + 48);
+
+                String num2 = String.copyValueOf(chars);
+
+                int x = Integer.parseInt(num2);
+
+                if(x % 88 == 0 && x > largestNumber){
+                    largestNumber = x;
+                }
+            }
+        }
+
+        String finalResult = String.valueOf(largestNumber);
+
+        String numAB = finalResult.charAt(0) + String.valueOf(finalResult.charAt(4));
+
+        int large = Integer.parseInt(numAB);
+
+        System.out.println("the final answer is AB = "+large);
+
+
+        //i was just getting the wrong answer even aftr debugging and seeing nothing wrong with my code.
+        //just read the question, and it is 88 and not 8.
+
+        //Might not be efficient because time complexity is O(n^2).
+
+
     }
 }
